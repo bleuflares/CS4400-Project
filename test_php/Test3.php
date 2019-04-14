37d057f62c8ddd602dd58574146fba561d910310
MOGEDI\mogedi <mahadgd94@gmail.com>
2019-04-13 23:12:20 -0400

    rename file directories

--- test_php/Test3.php
+++ test_php/Test3.php
@@ -106,6 +106,30 @@
     <script type="text/javascript">
     </script>
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "project";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "INSERT INTO users (Username, Firstname, Lastname, Status, Password, UserType)
+    VALUES (:userNameLabel, :firstNameLabel, :lastNameLabel, :'Active', :passwordLabel, :confirmPasswordLabel, :emailLabel)";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+?>
 </body>
 
 
