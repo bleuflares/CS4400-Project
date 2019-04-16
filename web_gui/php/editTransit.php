a11dd35dc2438b1d2bc43d6a0068fbb86ef38049
MOGEDI\mogedi <mahadgd94@gmail.com>
2019-04-15 22:09:26 -0400

    added DataTable Library CDN to file

--- web_gui/php/editTransit.php
+++ web_gui/php/editTransit.php
@@ -34,6 +34,8 @@ try {
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 
 
