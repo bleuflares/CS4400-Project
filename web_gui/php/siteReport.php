2591155de6794a7fe99c060cabf41402b6d9f1ae
MOGEDI\mogedi <mahadgd94@gmail.com>
2019-04-18 19:06:28 -0400

    added back button navigation to site report page

--- web_gui/php/siteReport.php
+++ web_gui/php/siteReport.php
@@ -22,7 +22,34 @@ try {
 }
 ?>
 
+<?php
 
+if (isset($_POST['backButton'])) {
+
+    $userType  = $_SESSION["userType"];
+
+    if (strpos($userType, "Employee") !== false && strpos($userType, "Visitor") === false) {
+        echo '<script>console.log("%cUser is EMPLOYEE", "color:blue")</script>';
+
+        if (strpos($_SESSION["user_employeeType"], "Manager") !== false) {
+            header('Location: http://localhost/web_gui/php/managerFunctionality.php');
+            exit();
+        } else {
+            echo '<script>console.log("%cUser is EMPLOYEE, BUT they are NOT a Admin, Manager, or Staff", "color:red")</script>';;
+        }
+    } else if (strpos($userType, "Employee") !== false && strpos($userType, "Visitor") !== false) {
+        echo '<script>console.log("%cUser is BOTH an EMPLOYEE and VISITOR", "color:blue")</script>';
+
+        if (strpos($_SESSION["user_employeeVisitorType"], "Manager") !== false) {
+            header('Location: http://localhost/web_gui/php/managerVisitorFunctionality.php');
+            exit();
+        } else {
+            echo '<script>console.log("%cUser is EMPLOYEE and VISITOR, BUT they are NOT a Admin, Manager, or Staff", "color:red")</script>';;
+        }
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -54,27 +81,25 @@ try {
 </head>
 
 <body>
-    <form class="form-signin">
+    <form class="form-signin" method="post">
         <h1 class="h3 mb-3 font-weight-heavy" id="titleOfForm">Site Report</h1>
-
 
         <div class="container">
             <div class="row">
                 <div class="col-sm-2 offset-0">
                     <label>Start Date</label>
                 </div>
+
                 <div class="col-sm-4 offset-0">
                     <input type="Date" class="form-control col-sm-0 offset-0" id="inputAdress">
-
                 </div>
-
 
                 <div class="col-sm-2 offset-0">
                     <label>End Date</label>
                 </div>
+
                 <div class="col-sm-4 offset-0">
                     <input type="Date" class="form-control col-sm-0 offset-0" id="inputAdress">
-
                 </div>
             </div>
 
@@ -82,6 +107,7 @@ try {
                 <div class="col-sm-0 offset-1">
                     <label>Event Count Range</label>
                 </div>
+
                 <div class="col-sm-3">
 
                     <input type="text" class="col-sm-1" style="text-align: center;" placeholder="">
@@ -91,10 +117,10 @@ try {
                     <input type="text" class="col-sm-1" style="text-align: center;" placeholder="">
                 </div>
 
-
                 <div class="col-sm-0 offset-0">
                     <label>Staff Count Range</label>
                 </div>
+
                 <div class="col-sm-3">
 
                     <input type="text" class="col-sm-1" style="text-align: center;" placeholder="">
@@ -111,6 +137,7 @@ try {
             <div class="col-sm-0 offset-1">
                 <label>Total Visits Range</label>
             </div>
+
             <div class="col-sm-3">
 
                 <input type="text" class="col-sm-1" style="text-align: center;" placeholder="">
@@ -119,7 +146,6 @@ try {
 
                 <input type="text" class="col-sm-1" style="text-align: center;" placeholder="">
             </div>
-
 
             <div class="col-sm-0 offset-0">
                 <label>Total Revenue Range</label>
@@ -134,13 +160,14 @@ try {
             </div>
 
         </div>
-        </div>
+
 
         <div class="row col-sm-12">
 
             <div class="col-sm-0 offset-2">
                 <button class="btn btn-sm btn-primary btn-block col-sm-0" style="border-radius: 5px;">Filter</button>
             </div>
+
             <div class="col-sm-0 offset-7">
                 <input id="button" class="btn btn-sm btn-primary btn-block col-sm-" type="submit" name="button"
                     onclick="myFunction();" value="Daily Detail" />
@@ -149,10 +176,6 @@ try {
 
 
         </div>
-        </div>
-
-
-
 
         <table id="test" class="table table-bordered" style="width:100%">
             <thead>
@@ -172,15 +195,13 @@ try {
 
         <div class="container">
             <div class="col-sm-2 offset-5">
-                <button class="btn btn-sm btn-primary btn-block col-sm-0" style="border-radius: 5px;">Back</button>
+                <button class="btn btn-sm btn-primary btn-block col-sm-0" style="border-radius: 5px;"
+                    name="backButton">Back</button>
             </div>
         </div>
 
     </form>
 
-
 </body>
-
-
 
 </html>
