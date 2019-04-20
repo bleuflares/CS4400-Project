823fc0820a37777975cb5fca2fa9ead50ffd3d62
MOGEDI\mogedi <mahadgd94@gmail.com>
2019-04-20 18:44:12 -0400

    fixed git merge issue

--- web_gui/php/manageStaff.php
+++ web_gui/php/manageStaff.php
@@ -1,7 +1,7 @@
 <?php
 // Start the session
 session_start();
-  $_SESSION['manageStaffFilterButton'] = false;
+$_SESSION['manageStaffFilterButton'] = false;
 
 if (!$_SESSION["logged_in"]) {
     header("Location: http://localhost/web_gui/php/userLogin.php");
@@ -23,11 +23,10 @@ try {
 }
 
 
-if (isset($_POST['filterButton'])){
+if (isset($_POST['filterButton'])) {
     echo '<script>console.log("%cSuccessful Filter Button Push", "color:blue")</script>';
     $_SESSION['manageStaffFilterButton'] = True;
     echo '<script>console.log("%c Transit History Filter Session variable set", "color:blue")</script>';
-
 }
 
 ?>
@@ -61,13 +60,6 @@ if (isset($_POST['backButton'])) {
 
 ?>
 
-<?php
-
-if (isset($_POST['filterButton'])) { }
-
-
-?>
-
 
 
 
@@ -77,12 +69,6 @@ if (isset($_POST['filterButton'])) { }
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<<<<<<< HEAD
-   <!--  <meta http-equiv="refresh" content="3"> -->
-=======
-    <!-- <meta http-equiv="refresh" content="3"> -->
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
-
     <link rel="stylesheet" href="..\css\_universalStyling.css">
 
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
@@ -91,7 +77,7 @@ if (isset($_POST['filterButton'])) { }
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 
- <!--    <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+    <!--    <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
 
     <script type="text/javascript">
@@ -118,14 +104,13 @@ if (isset($_POST['filterButton'])) { }
         <h1 class="h3 mb-3 font-weight-heavy" id="titleOfForm">Manage Staff</h1>
 
         <div class="container">
+
             <div class="row">
+
                 <div class="col-sm-12 offset-0">
+
                     <label class='offset-4'>Site</label>
-<<<<<<< HEAD
-                    <select name = "siteName">
-=======
-                    <select name="siteInput">
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
+                    <select name="siteName">
                         <?php
                         $result = $conn->query("SELECT SiteName FROM Site");
 
@@ -135,131 +120,106 @@ if (isset($_POST['filterButton'])) { }
                         echo "<option>ALL</option>";
                         ?>
                     </select>
-                    <div class="row">
-                        <div class="col-sm-0 offset-0">
-                            <label>First Name</label>
-                        </div>
-                        <div class="col-sm-3 offset-0">
-<<<<<<< HEAD
-                            <input type="text" class="form-control col-sm-0 offset-0" id="inputAdress" name = "firstName">
-=======
-                            <input type="text" class="form-control col-sm-0 offset-0" name="fnameInput">
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
 
-                        </div>
+                </div>
 
+                <div class="row">
+                    <div class="col-sm-0 offset-0">
+                        <label>First Name</label>
+                    </div>
+                    <div class="col-sm-3 offset-0">
+                        <input type="text" class="form-control col-sm-0 offset-0" id="inputAdress" name="firstName">
 
-                        <div class="col-sm-2 offset-0">
-                            <label>Last Name</label>
-                        </div>
-                        <div class="col-sm-3 offset-0">
-<<<<<<< HEAD
-                            <input type="text" class="form-control col-sm-0 offset-0" id="inputAdress" name ="lastName">
-=======
-                            <input type="text" class="form-control col-sm-0 offset-0" name="lnameInput">
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
-
-                        </div>
                     </div>
 
-                    <div class="row">
-                        <div class="col-sm-0 offset-0">
-                            <label>Start Date</label>
 
-<<<<<<< HEAD
-                            <input type="date" class="col-sm-0" style="padding: 0;" placeholder="" name = "startDate">
-=======
-                            <input type="date" class="col-sm-0" style="padding: 0;" name="startDateInput">
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
-
-                        </div>
-
-                        <div class="col-sm-0 offset-1">
-                            <label>End Date</label>
-
-<<<<<<< HEAD
-                            <input type="date" class="col-sm-0" style="padding: 0;" placeholder="" name = "endDate">
-=======
-                            <input type="date" class="col-sm-0" style="padding: 0;" name="endDateInput">
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
-
-                        </div>
+                    <div class="col-sm-2 offset-0">
+                        <label>Last Name</label>
                     </div>
+                    <div class="col-sm-3 offset-0">
 
-                    <div class="row col-sm-12">
-
-                        <div class="col-sm-0 offset-6">
-                            <button class="btn btn-sm btn-primary btn-block col-sm-0  " style=" height:40px;
-<<<<<<< HEAD
-                             width:60px;border-radius: 5px;" name ="filterButton">Filter</button>
-=======
-    width:60px;border-radius: 5px;" name="filterButton">Filter</button>
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
-                        </div>
+                        <input type="text" class="form-control col-sm-0 offset-0" id="inputAdress" name="lastName">
 
                     </div>
                 </div>
+
+                <div class="row">
+                    <div class="col-sm-0 offset-0">
+                        <label>Start Date</label>
+
+                        <input type="date" class="col-sm-0" style="padding: 0;" placeholder="" name="startDate">
+
+                    </div>
+
+                    <div class="col-sm-0 offset-1">
+                        <label>End Date</label>
+
+                        <input type="date" class="col-sm-0" style="padding: 0;" placeholder="" name="endDate">
+
+
+                    </div>
+                </div>
+
+                <div class="row col-sm-12">
+
+                    <div class="col-sm-0 offset-6">
+                        <button class="btn btn-sm btn-primary btn-block col-sm-0  "
+                            style=" height:40px; width:60px;border-radius: 5px;" name="filterButton">Filter</button>
+                    </div>
+
+                </div>
             </div>
+        </div>
 
-            <table id="test" class="table table-bordered" style="width:100%">
-                <thead>
-                    <tr>
-                        <th style='text-align:center'>Staff Name</th>
-                        <th style='text-align:center'># of Event Shifts Shifts</th>
+        <table id="test" class="table table-bordered" style="width:100%">
+            <thead>
+                <tr>
+                    <th style='text-align:center'>Staff Name</th>
+                    <th style='text-align:center'># of Event Shifts Shifts</th>
 
-                    </tr>
-                </thead>
+                </tr>
+            </thead>
 
-                <tbody>
+            <tbody>
 
-<<<<<<< HEAD
-                    <?php
+                <?php
 
-                    if ($_SESSION['manageStaffFilterButton'] == True){
-                            echo '<script>console.log("%cSuccessful Filter", "color:blue")</script>';
+                if ($_SESSION['manageStaffFilterButton'] == True) {
+                    echo '<script>console.log("%cSuccessful Filter", "color:blue")</script>';
 
-                    
+                    $siteName  = $_POST['siteName'];
 
-                        $siteName  = $_POST['siteName'];
+                    if (empty($_POST['firstName'])) {
+                        $firstName = "%%";
+                    } else {
+                        $firstName = $_POST['firstName'];
+                    }
+
+                    if (empty($_POST['lastName'])) {
+                        $lastName = "%%";
+                    } else {
+                        $lastName = $_POST['firstName'];
+                    }
+                    if (empty($_POST['startDate'])) {
+                        $startDate = "0000-00-00";
+                    } else {
+                        $startDate = $_POST['startDate'];
+                    }
+
+                    if (empty($_POST['endDate'])) {
+                        $endDate = "9999-12-12";
+                    } else {
+                        $endDate = $_POST['endDate'];
+                    }
+                    echo '<script>console.log("Works Input: ' . $siteName . '")</script>';
+                    echo '<script>console.log("Works Input: ' . $firstName . '")</script>';
+                    echo '<script>console.log("Works Input: ' . $lastName . '")</script>';
+                    echo '<script>console.log("Works Input: ' . $startDate . '")</script>';
+                    echo '<script>console.log("Works Input: ' . $endDate . '")</script>';
 
 
 
-
-                        if (empty($_POST['firstName'])) {
-                            $firstName = "%%";
-
-                        } else {
-                            $firstName = $_POST['firstName'];
-                        }
-
-                        if (empty($_POST['lastName'])) {
-                            $lastName = "%%";
-
-                        } else {
-                            $lastName = $_POST['firstName'];
-                        }
-                        if (empty($_POST['startDate'])) {
-                            $startDate = "0000-00-00";
-                        } else {
-                            $startDate = $_POST['startDate'];
-                            
-                        }
-
-                        if (empty($_POST['endDate'])) {
-                            $endDate = "9999-12-12";
-                           
-                        } else {
-                            $endDate = $_POST['endDate'];
-                        }
-                echo '<script>console.log("Works Input: ' . $siteName . '")</script>';
-                echo '<script>console.log("Works Input: ' . $firstName . '")</script>';
-                 echo '<script>console.log("Works Input: ' . $lastName . '")</script>';
-                echo '<script>console.log("Works Input: ' . $startDate . '")</script>';
-                 echo '<script>console.log("Works Input: ' . $endDate . '")</script>';
-
-                 
-
-                $result = $conn->query("SELECT concat(user.firstname, ' ',user.lastname) as staffName, count(*) as eventShifts
+                    $result = $conn->query("SELECT concat(user.firstname, ' ',user.lastname) as staffName, count(*) as eventShifts
                                             FROM event 
                                             left join assignto on event.eventName = assignTo.eventName
                                             and event.startDate = assignTo.startDate
@@ -274,78 +234,45 @@ if (isset($_POST['filterButton'])) { }
                                             group by user.username
                                             ;");
 
+                    while ($row = $result->fetch()) {
 
-
-
-
-
-
-                    while ($row = $result->fetch()) { 
-                           
                         echo "<tr>";
                         echo   "<td style='text-align:center'>" . $row['staffName'] . "</td>";
                         echo "<td style='text-align:center'>" . $row['eventShifts'] . "</td>";
-                            
+
 
                         echo "<tr>";
-
-                        }
-
                     }
-                    else{$result = $conn->query("SELECT concat(user.firstname, ' ',user.lastname) as staffName, 
-                    count(*) as eventShifts
-                    FROM event 
-                    left join assignto on event.eventName = assignTo.eventName
-                    and event.startDate = assignTo.startDate
-                    and event.siteName = assignTo.siteName
-                    left join employee on assignTo.staffUsername = employee.username
-                    left join user on employee.username = user.username
-                    group by assignTo.staffUsername;");
-=======
+                } else {
+                    $result = $conn->query("SELECT concat(user.firstname, ' ',user.lastname) as staffName, 
+                                                count(*) as eventShifts
+                                                FROM event 
+                                                left join assignto on event.eventName = assignTo.eventName
+                                                and event.startDate = assignTo.startDate
+                                                and event.siteName = assignTo.siteName
+                                                left join employee on assignTo.staffUsername = employee.username
+                                                left join user on employee.username = user.username
+                                                group by assignTo.staffUsername;");
 
-                    <!-- else {$result = $conn->query("SELECT c.transitRoute, c.transitType,tt.transitPrice, c.connectedSites,tt.totalRiders
-                        FROM (select c.siteName, c.transitType, c.transitRoute, count(*) as connectedSites
-                        from connect c
-                        group by transitRoute) as c
-                        Join
-                        (select  t.transitRoute, t.transitPrice,
-                        count(*) as totalRiders 
-                        from taketransit tt
-                        inner join transit t
-                        on t.transitRoute = tt.transitRoute
-                        group by transitRoute) as tt
-                        where c.transitRoute = tt.transitRoute;");
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
 
-                    
+                    while ($row = $result->fetch()) {
 
-                        while ($row = $result->fetch()) { 
-                           
-                            echo "<tr>";
-                            echo   "<td style='text-align:center'>" . $row['staffName'] . "</td>";
-                            echo "<td style='text-align:center'>" . $row['eventShifts'] . "</td>";
-                            
+                        echo "<tr>";
+                        echo   "<td style='text-align:center'>" . $row['staffName'] . "</td>";
+                        echo "<td style='text-align:center'>" . $row['eventShifts'] . "</td>";
 
-                            echo "<tr>";
 
-                        }
-<<<<<<< HEAD
+                        echo "<tr>";
                     }
+                }
+                ?>
+            </tbody>
+        </table>
 
-
-                        ?>
-            
-=======
-            } -->
->>>>>>> a7e06bb2098fcc13901c6c86ec50ba1097ef5bdf
-                </tbody>
-            </table>
-
-            <div class="container">
-                <div class="col-sm-2 offset-5">
-                    <button class="btn btn-sm btn-primary btn-block col-sm-0" style="border-radius: 5px;"
-                        name="backButton">Back</button>
-                </div>
+        <div class="container">
+            <div class="col-sm-2 offset-5">
+                <button class="btn btn-sm btn-primary btn-block col-sm-0" style="border-radius: 5px;"
+                    name="backButton">Back</button>
             </div>
         </div>
 
